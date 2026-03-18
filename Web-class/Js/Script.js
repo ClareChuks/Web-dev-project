0167487e7c2ae1abc9302b64276e86b673af36db
Clare Chuks <clarechuksajemba@gmail.com>
2026-03-18 18:21:30 +0100

    Delete Web-class directory

--- Web-class/Js/Script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  if (typeof AOS !== "undefined") {
-    AOS.init({
-      duration: 800,
-      easing: "ease-in-out",
-      once: true,
-    });
-  }
-});
