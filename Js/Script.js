ec0bdcf07fa471f132fe50d79a6add1be90c30e2
clare chuks <clarechuksajemba@gmail.com>
2026-04-05 22:37:40 +0100

    Mobile reponsiveness and font awesome icons added to the project.

--- Js/Script.js
+++ Js/Script.js
@@ -7,3 +7,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
